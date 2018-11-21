5840923c1cd50d73a630fff8a431fd8c9475ad89
Dan Farrell <dfarrell@doctorondemand.com>
2018-11-21 12:16:55 -0600

    fixup

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource "google_container_cluster" "dod-cluster-1" {
   node_config {
     disk_size_gb  = "100"
     oauth_scopes = [
-      "https://www.googleapis.com/auth/devstorage.read_only"
+      "https://www.googleapis.com/auth/devstorage.read_only",
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
       "https://www.googleapis.com/auth/ndev.clouddns.readwrite",
